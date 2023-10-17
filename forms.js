723a7b162588df24d4aed43f5cd400ca25f4aa09
Vishal Agarwala <agarwala@me.com>
2023-10-17 16:35:17 -0400

    Formatted document

--- forms.js
+++ forms.js
@@ -1,11 +1,11 @@
 var Webflow = Webflow || [];
-Webflow.push(function() {
+Webflow.push(function () {
   // unbind webflow form handling (keep this if you only want to affect specific forms)
   $(document).off('submit');
   /* Any form on the page */
-  $('.cf-replacement').submit(function(e) {
+  $('.cf-replacement').submit(function (e) {
     e.preventDefault();
-  	const $form = $(this); // The submitted form
+    const $form = $(this); // The submitted form
     const $submit = $('[type=submit]', $form); // Submit button of form
     const buttonText = $submit.val(); // Original button text
     const buttonWaitingText = $submit.attr('data-wait'); // Waiting button text value
@@ -21,75 +21,75 @@ Webflow.push(function() {
     if (buttonWaitingText) {
       $submit.val(buttonWaitingText);
     }
-	
-	let submission = new Promise(function(resolve, reject) {
-	    // Get the form data from the event object
-	    let obj = {};
-	    let email;
 
-	    let entries = [...formData.entries()];
-		
-	    entries.forEach((entry) => (obj[entry[0]] = entry[1]));
-	    email = obj["email"];
-	    delete obj.email;
+    let submission = new Promise(function (resolve, reject) {
+      // Get the form data from the event object
+      let obj = {};
+      let email;
 
-	    // Get UTM cookie data
-	    let cookies = document.cookie.split(';');
-	    let utmCookies = {};
+      let entries = [...formData.entries()];
 
-	    cookies.forEach(cookie => {
-	      let [name, value] = cookie.split('=').map(s => s.trim());
-	      if (name.includes('utm')) {
-	        utmCookies[name] = decodeURIComponent(value);
-	      }
-	    });
+      entries.forEach((entry) => (obj[entry[0]] = entry[1]));
+      email = obj["email"];
+      delete obj.email;
 
-	    // Send data to Segment
+      // Get UTM cookie data
+      let cookies = document.cookie.split(';');
+      let utmCookies = {};
+
+      cookies.forEach(cookie => {
+        let [name, value] = cookie.split('=').map(s => s.trim());
+        if (name.includes('utm')) {
+          utmCookies[name] = decodeURIComponent(value);
+        }
+      });
+
+      // Send data to Segment
       analytics.identify(email);
       analytics.identify({
-            email: email,
-            last_form_submission_confirmation_url: obj["last_form_submission_confirmation_url"],
-            last_form_submission_name: $form.get(0).attributes[2].nodeValue,
-            last_submission_url: document.URL
-        });
-	    analytics.track("Form Submission", {
-            form_name: $form.get(0).attributes[2].nodeValue,
-            email: email,
-            fields: obj,
-            url: document.URL,
-            utm: utmCookies
-	    });
+        email: email,
+        last_form_submission_confirmation_url: obj["last_form_submission_confirmation_url"],
+        last_form_submission_name: $form.get(0).attributes[2].nodeValue,
+        last_submission_url: document.URL
+      });
+      analytics.track("Form Submission", {
+        form_name: $form.get(0).attributes[2].nodeValue,
+        email: email,
+        fields: obj,
+        url: document.URL,
+        utm: utmCookies
+      });
 
       // Add email to local storage
       localStorage.setItem('email_given', email);
-		
-		  setTimeout(function() {
-		    resolve('Success!');
-		  }, 1000);
-	});
 
-	submission.then(function(result) {
-	    // If form redirect setting set, then use this and prevent any other actions
-	    if (formRedirect) { window.location = formRedirect; return; }
-	  	$form
-	    	.hide() // optional hiding of form
-	  		.siblings('.w-form-done').show() // Show success
-	    	.siblings('.w-form-fail').hide(); // Hide failure
-      
+      setTimeout(function () {
+        resolve('Success!');
+      }, 1000);
+    });
+
+    submission.then(function (result) {
+      // If form redirect setting set, then use this and prevent any other actions
+      if (formRedirect) { window.location = formRedirect; return; }
+      $form
+        .hide() // optional hiding of form
+        .siblings('.w-form-done').show() // Show success
+        .siblings('.w-form-fail').hide(); // Hide failure
+
       // If submitting a form to allow for /tools usage
       if ($('.tool-popover_component').hasClass('tool-popover_component')) {
         $('.tool-popover_component').hide();
       }
-        
-      }).catch(function(error) {
-        console.log(error);
-        $('.error-message-text').text(error);
-          $form
-           .siblings('.w-form-done').hide() // Hide success
-           .siblings('.w-form-fail').show(); // show failure
-      }).finally(function() {
-          // Reset text
-          $submit.val(buttonText);
-      });
+
+    }).catch(function (error) {
+      console.log(error);
+      $('.error-message-text').text(error);
+      $form
+        .siblings('.w-form-done').hide() // Hide success
+        .siblings('.w-form-fail').show(); // show failure
+    }).finally(function () {
+      // Reset text
+      $submit.val(buttonText);
+    });
   });
 });
